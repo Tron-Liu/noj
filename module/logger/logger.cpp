9d25b87cd141e7885f7f6b77a7aa9641f8f63d81
Tron-Liu <Tron-Liu@foxmail.com>
2024-05-26 10:59:04 +0800

    1. clean header file 2. use macro wrapping function calls 3. hungry singleton mode

--- module/logger/logger.cpp
+++ module/logger/logger.cpp
@@ -1,32 +1,34 @@
 #include "logger.h"
-#include "path.h"
+
+#include "spdlog/async.h"
+#include "spdlog/common.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+
 #include <cstring>
-#include <filesystem>
 #include <string>
 #include <unistd.h>
 
 namespace TL {
 
-Logger* Logger::instance_ = nullptr;
-
-Logger::Logger()
-{
-  std::string homePath = Path::getHome();
-  std::string filename = homePath + "/" + "logs" + "/" + fileName_;
-
-  logger_ = spdlog::daily_logger_mt(loggerName_, filename, 0, 30);
-}
-
 Logger* Logger::GetInstance()
 {
-  if (instance_ == nullptr) {
-    instance_ = new Logger();
-    return instance_;
-  }
-
-  return instance_;
+  static Logger instance;
+  return &instance;
 }
 
-LoggerPtr Logger::GetLogger() { return logger_; }
+std::shared_ptr<spdlog::logger> Logger::GetLogger() { return logger_; }
+
+void Logger::Init(LogConfig config)
+{
+
+  logger_ = spdlog::rotating_logger_mt<spdlog::async_factory>(
+    loggerName_, config.path, config.size, config.count);
+
+  logger_->set_pattern(
+    "[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [thread %t] [%s %!:%#] %v");
+
+  logger_->set_level(spdlog::level::from_str(config.level));
+  logger_->flush_on(spdlog::level::from_str(config.level));
+}
 
 } // namespace TL
--- module/logger/logger.h
+++ module/logger/logger.h
@@ -1,37 +1,59 @@
 #pragma once
 
-#include <filesystem>
-#include <iostream>
+#include "spdlog/logger.h"
+
+#include <cstdint>
 #include <memory>
 #include <string>
 
-#include "spdlog/logger.h"
-#include "spdlog/sinks/daily_file_sink.h"
-#include "spdlog/spdlog.h"
-
-#include "path.h"
-
 namespace TL {
 
-typedef std::shared_ptr<spdlog::logger> LoggerPtr;
+struct LogConfig {
+  std::string level;
+  std::string path;
+  uint64_t size;
+  int count;
+};
 
 class Logger {
 private:
-  const int BUFFER_SIZE = 512;
+  const std::string loggerName_ = "NOJ";
 
-  const std::string loggerName_ = "NOJLogger";
-  const std::string fileName_   = "daily.txt";
-
-  static Logger* instance_;
   std::shared_ptr<spdlog::logger> logger_;
 
 private:
-  Logger();
-  ~Logger();
+  Logger() = default;
 
 public:
   static Logger* GetInstance();
-  LoggerPtr GetLogger();
+  std::shared_ptr<spdlog::logger> GetLogger();
+
+  void Init(LogConfig config);
 };
+
+#define INITLOG(config) Logger::GetInstance()->Init(config);
+
+#define BASELOG(logger, level, ...)                                            \
+  (logger)->log(                                                               \
+    spdlog::source_loc { __FILE__, __LINE__, __func__ }, level, __VA_ARGS__)
+
+#define LOG_TRACE(...)                                                         \
+  BASELOG(Logger::GetInstance()->GetLogger(), spdlog::level::trace, __VA_ARGS__)
+
+#define LOG_DEBUG(...)                                                         \
+  BASELOG(Logger::GetInstance()->GetLogger(), spdlog::level::debug, __VA_ARGS__)
+
+#define LOG_INFO(...)                                                          \
+  BASELOG(Logger::GetInstance()->GetLogger(), spdlog::level::info, __VA_ARGS__)
+
+#define LOG_WARN(...)                                                          \
+  BASELOG(Logger::GetInstance()->GetLogger(), spdlog::level::warn, __VA_ARGS__)
+
+#define LOG_ERROR(...)                                                         \
+  BASELOG(Logger::GetInstance()->GetLogger(), spdlog::level::err, __VA_ARGS__)
+
+#define LOG_CRITICAL(...)                                                      \
+  BASELOG(                                                                     \
+    Logger::GetInstance()->GetLogger(), spdlog::level::critical, __VA_ARGS__)
 
 } // namespace TL
